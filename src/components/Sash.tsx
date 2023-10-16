1d9bfc76ad56635fc6cf1f2033e06684d7bca726
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2023-10-16 08:58:25 -0300

    chore: Code adjustments

--- src/components/Sash.tsx
+++ src/components/Sash.tsx
@@ -2,7 +2,7 @@
 export const Sash = () => {
 
   return (
-    <section className="flex mx-auto justify-center items-center h-40 w-5/6 mb-10 bg-color-sash rounded-3xl shadow-md">
+    <section className="flex mx-auto justify-center items-center h-40 w-4/6 mb-10 bg-color-sash rounded-3xl shadow-md">
       <h2 className="text-7xl text-white font-semibold"> 
         Nossos Serviços
       </h2>
