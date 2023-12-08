8e885cb1bc9144836025435249f257469a86c416
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2023-12-08 11:39:15 -0300

    chore: update icons

--- src/app/servicos/[servicoId]/page.tsx
+++ src/app/servicos/[servicoId]/page.tsx
@@ -3,6 +3,7 @@
 import { servicesData } from "@/data/servicesData";
 import Link from "next/link";
 import { useState } from "react";
+import { FaArrowCircleLeft , FaArrowCircleRight } from "react-icons/fa";
 
 export default function HomeServicoId({ params }: { params: { servicoId: string } }) {
 
@@ -40,9 +41,33 @@ export default function HomeServicoId({ params }: { params: { servicoId: string 
             
             <h2 className="my-2 text-4xl font-bold text-center"> {servico.title} </h2>
 
-            <img className="w-28 mx-auto my-5"
-            src= {servico.photos[imageIndex]}
-            alt= {`Carrosel de Imagens de ${servico.title}.`} />
+            <div className="relative">
+                <FaArrowCircleLeft onClick={prevImage} 
+                    style={{ 
+                        position: "absolute", 
+                        left: 80, 
+                        top: "50%", 
+                        transform: "translateY(-50%)", 
+                        cursor: "pointer", 
+                        fontSize: '2em'
+                        }}
+                    />
+
+                <img className="w-28 mx-auto my-5"
+                src= {servico.photos[imageIndex]}
+                alt= {`Carrosel de Imagens de ${servico.title}.`} />
+
+                <FaArrowCircleRight onClick={nextImage} 
+                    style={{ 
+                        position: "absolute", 
+                        right: 80, 
+                        top: "50%", 
+                        transform: "translateY(-50%)", 
+                        cursor: "pointer", 
+                        fontSize: '2em',        
+                        }}
+                    />
+            </div>
 
             <p className="my-5 text-xl text-center"> {servico.text} </p>
             
@@ -50,9 +75,6 @@ export default function HomeServicoId({ params }: { params: { servicoId: string 
             target="_blank" 
             href={`https://wa.me/5511974276250/?text=Ol%C3%A1+Vidra%C3%A7aria+Artes+Vidros+W.R.R.+Quero+Solicitar+um+Or%C3%A7amento+Gratuito+de+${servico.title}.`} 
             className='flex mx-auto my-3 text-2xl rounded-3xl shadow-md text-white p-3 border-2 bg-black font-mono font-bold'>Solicitar Orçamento</Link>
-
-            <button onClick={nextImage}>next</button>
-            <button onClick={prevImage}>prev </button>
 
         </main> 
 
