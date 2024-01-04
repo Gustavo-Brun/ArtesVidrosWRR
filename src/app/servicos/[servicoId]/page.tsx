1a3df2edbf396182dbe84f257f0c202fef197cfd
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-01-04 12:23:27 -0300

    chore: Update Single Service Page

--- src/app/servicos/[servicoId]/page.tsx
+++ src/app/servicos/[servicoId]/page.tsx
@@ -1,9 +1,5 @@
-'use client'
-
 import { servicesData } from "@/data/servicesData";
-import Link from "next/link";
-import { useState } from "react";
-import { FaArrowCircleLeft , FaArrowCircleRight } from "react-icons/fa";
+import Link from "next/link";;
 
 export default function HomeServicoId({ params }: { params: { servicoId: string } }) {
 
@@ -11,62 +7,24 @@ export default function HomeServicoId({ params }: { params: { servicoId: string 
 
     if (!servico) {
         return (
-        <div className={"text-3xl font-medium mx-auto text-center mt-20 p-6 border-2 border-black rounded-xl shadow-2xl bg-white w-2/4"}>
+        <div className={"text-3xl font-medium mx-auto text-center my-48 p-6 border-2 border-black rounded-xl shadow-2xl bg-white w-2/4"}>
             Serviço não encontrado
         </div>
         )
       }
 
-    const [ imageIndex, setImageIndex ] = useState(0)
-
-    const nextImage = () => {
-        
-        if (imageIndex < servico.photos.length -1) {        
-            setImageIndex(imageIndex+1)
-        } 
-    }
-
-    const prevImage = () => {
-        
-        if (imageIndex > 0 ) {
-            setImageIndex(imageIndex-1)
-        } else {
-            setImageIndex(0)
-        }
-    }
-    
     return (
         
         <main className= {"grid grid-cols-1 gap-2 mx-auto my-3 p-6 border-2 border-black rounded-xl shadow-2xl bg-white w-2/4"}>
             
             <h2 className="my-2 text-4xl font-bold text-center"> {servico.title} </h2>
 
-            <div className="relative">
-                <FaArrowCircleLeft onClick={prevImage} 
-                    style={{ 
-                        position: "absolute", 
-                        left: 80, 
-                        top: "50%", 
-                        transform: "translateY(-50%)", 
-                        cursor: "pointer", 
-                        fontSize: '2em'
-                        }}
-                    />
+            <div>
 
                 <img className="w-28 mx-auto my-5"
-                src= {servico.photos[imageIndex]}
+                src= {servico.photos[0]}
                 alt= {`Carrosel de Imagens de ${servico.title}.`} />
 
-                <FaArrowCircleRight onClick={nextImage} 
-                    style={{ 
-                        position: "absolute", 
-                        right: 80, 
-                        top: "50%", 
-                        transform: "translateY(-50%)", 
-                        cursor: "pointer", 
-                        fontSize: '2em',        
-                        }}
-                    />
             </div>
 
             <p className="my-5 text-xl text-center"> {servico.text} </p>
