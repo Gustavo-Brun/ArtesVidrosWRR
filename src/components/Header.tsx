6fa3c3a7ad2b4b6463290230e4d1c2e5a8edd0c5
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-01-16 14:24:15 -0300

    chore: Change Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,27 +20,37 @@ export const Header = () => {
                 <h1 className="text-6xl mb-4">Vidraçaria Artes Vidros W.R.R</h1>
                 <p>Vidraçaria Profissional no Jardim Selma - Zona Sul de São Paulo</p>
             </div>
-            <Link 
-            target="_blank" 
-            href={'https://wa.me/5511974276250/?text=Ol%C3%A1+Vidra%C3%A7aria+Artes+Vidros+W.R.R.+Quero+Solicitar+um+Or%C3%A7amento+Gratuito.'}
+            <div 
             className="before:border-t-2 before:block before:mb-10 before:border-gray-500"
             >
-                <div className="flex text-center items-center">
-                    <FaWhatsapp 
-                    style={{ background: 'linear-gradient(45deg, #25D366, #128C7E)' , borderRadius: '50%', fontSize: '70px' , padding: '10px' }} 
-                    />
+                <div className="flex text-center items-center space-x-6 ">
+                    <Link
+                    target="_blank" 
+                    href={'https://wa.me/5511974276250/?text=Ol%C3%A1+Vidra%C3%A7aria+Artes+Vidros+W.R.R.+Quero+Solicitar+um+Or%C3%A7amento+Gratuito.'}
+                    className="ml-6 animate-pulse active:text-color-gray"
+                    >
+                        <FaWhatsapp
+                        style={{ background: 'linear-gradient(45deg, #25D366, #128C7E)' , borderRadius: '50%', fontSize: '70px' , padding: '10px' }}
+                        />
+                    </Link>
 
-                    <div className="ml-5">
+                    <Link 
+                    href="tel:+5511980977240"
+                    className="space-y-2 active:text-green-600"
+                    >
                         <p className="text-lg my-2">
-                            Agende seu Orçamento Gratuito:
+                            Agende seu Orçamento Gratuito: 
                         </p>
                         <p className="text-4xl">
                             (11) 97427-6250
                         </p>
-                    </div>
+                        <p className="text-4xl">
+                            (11) 98097-7240
+                        </p>
+                    </Link>
                 </div>
                 
-            </Link>
+            </div>
         </div>
     </header>
   )
