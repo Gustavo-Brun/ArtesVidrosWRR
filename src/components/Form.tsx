d09bea45cfd9c5bccd3ad83eee32a234abce3a70
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-01-16 22:45:08 -0300

    feat: Add Database Connection

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,15 +5,35 @@ import { useForm } from "react-hook-form"
 export const Form = () => {
 
     interface FormProps {
-        name: string ;
+        nome: string ;
         contato: string ;
-        msg: string;
+        mensagem: string;
       }
 
     const { register , handleSubmit, formState: { errors } } = useForm<FormProps>()
 
-    const onSubmit =  (data:FormProps) => {
-        //Enviar para banco de dados
+    const onSubmit = async (data:FormProps) => {
+
+        try {
+            const response = await fetch('https://apiartesvidroswrr.onrender.com/clientes/cadastro', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+    
+            if (response.ok) {
+                // Dados enviados com sucesso
+                window.alert('Recebemos seu registro, logo entraremos em contato! :)')
+            } else {
+                // Lidar com erro na resposta do servidor
+                window.alert('Ops! Houve um problema. Entre em contato através do nosso WhatsApp! Aguardamos seu contato.')
+            }
+        } catch (error) {
+            // Lidar com erro na requisição
+            window.alert('Ops! Houve um problema. Entre em contato através do nosso WhatsApp! Aguardamos seu contato.')
+        }
     }
 
     return (
@@ -22,10 +42,10 @@ export const Form = () => {
 
                 <div className="flex flex-col gap-1">
                     <label htmlFor="">Nome</label>
-                    <input {...register('name', {required:true})} 
-                    className={` border shadow-sm rounded h-10 p-3 font-normal ${errors?.name ? 'border-red-600 ' : 'border-black'}`}
+                    <input {...register('nome', {required:true})} 
+                    className={` border shadow-sm rounded h-10 p-3 font-normal ${errors?.nome ? 'border-red-600 ' : 'border-black'}`}
                      type="text" placeholder="Seu nome"/>
-                    {errors?.name?.type === 'required' && <p className="text-sm text-red-700">O nome é obrigatório.</p>}
+                    {errors?.nome?.type === 'required' && <p className="text-sm text-red-700">O nome é obrigatório.</p>}
                 </div>
 
                 <div className="flex flex-col gap-1">
@@ -38,11 +58,11 @@ export const Form = () => {
 
                 <div className="flex flex-col gap-1">
                     <label htmlFor="paragraph_text">Mensagem</label>
-                    <textarea {...register('msg', {required:true})}
-                    className={`border shadow-sm rounded p-3 font-normal ${errors?.msg ? 'border-red-600 ' : 'border-black'}`} id="paragraph_text"
+                    <textarea {...register('mensagem', {required:true})}
+                    className={`border shadow-sm rounded p-3 font-normal ${errors?.mensagem ? 'border-red-600 ' : 'border-black'}`} id="paragraph_text"
                     rows={3}
                     placeholder="Sua mensagem aqui"></textarea>
-                    {errors?.msg?.type === 'required' && <p className="text-sm text-red-700">Escreva uma mensagem!</p>}
+                    {errors?.mensagem?.type === 'required' && <p className="text-sm text-red-700">Escreva uma mensagem!</p>}
                 </div>
 
                 <button onClick={() => handleSubmit(onSubmit)()} className="mx-auto my-4 rounded-2xl shadow-md text-white p-3 px-8 border-2 bg-black  font-bold active:text-red-600 active:scale-95">Enviar</button>
