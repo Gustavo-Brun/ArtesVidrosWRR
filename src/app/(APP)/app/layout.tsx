1728e070e990aa0178640b67303db02c8319be1f
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-07-01 11:28:30 -0300

    feat: implement authentication check in the app route

--- src/app/(APP)/app/layout.tsx
+++ src/app/(APP)/app/layout.tsx
@@ -1,8 +1,18 @@
-export default function RootLayout({
+import { auth } from '@/services/auth'
+
+import { Unauthorized } from '@/components/Unauthorized';
+
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await auth()
+
+    if (!session) {
+        return <Unauthorized />
+    }
+
     return (
         <>
             {children}
--- src/app/(APP)/app/pdf/page.tsx
+++ src/app/(APP)/app/pdf/page.tsx
@@ -1,16 +1,6 @@
-import { auth } from '@/services/auth'
-
-import { Unauthorized } from '@/components/Unauthorized';
 import { DataRequest } from "@/components/DataRequest";
 
 export default async function HomePdf() {
-
-  const session = await auth()
-
-  if (!session) {
-    return <Unauthorized />
-  }
-
   return (
     <main>
       <DataRequest />
