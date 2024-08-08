2874eb896dbadd0ee84ef663b65132a981569c5f
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-08-08 14:13:52 -0300

    refactor: increase font size of pdf file

--- src/components/AppPages/Receipt/GeneratePDF.tsx
+++ src/components/AppPages/Receipt/GeneratePDF.tsx
@@ -31,21 +31,21 @@ export const Pdf: React.FC<PdfProps> = ({ userData, generatePDF, contentRef }) =
 
                 <header className='my-8 w-full flex justify-between'>
                     <div>
-                        <h1 className='text-2xl font-bold'>Recibo de Pagamento</h1>
-                        <p className='text-lg text italic'>Emissão: <span>{new Date().toLocaleDateString('pt-BR')}</span></p>
+                        <h1 className='text-3xl font-bold'>Recibo de Pagamento</h1>
+                        <p className='text-xl text italic'>Emissão: <span>{new Date().toLocaleDateString('pt-BR')}</span></p>
                     </div>
                     <div>
-                        <h2 className='text-4xl font-bold underline'>Valor: R${userData.value},00</h2>
+                        <h2 className='text-5xl font-bold underline'>Valor: R${userData.value},00</h2>
                     </div>
                 </header>
 
-                <div className='flex flex-col gap-2'>
+                <div className='text-lg flex flex-col gap-2'>
                     <p> Recebemos de <span className='font-semibold'>{userData.name}</span>{cnpj !== null && cnpj !== undefined && cnpj.length >= 1 && <span className="font-semibold">, portador do CPF/CNPJ n° {cnpj},</span>} a importância de <span className="font-semibold">{extenso(userData.value, { mode: "currency" })}</span> referente a(o) <span className="font-semibold">{userData.regarding}</span>.</p>
                     <p>Para maior clareza, firmamos o presente recibo, que comprova o recebimento integral do valor mencionado, concedendo quitação plena, geral e irrevogável pela quantia recebida.</p>
                 </div>
 
 
-                <span className='text-lg text-end'>São Paulo, {date.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' })}</span>
+                <span className='text-xl text-end'>São Paulo, {date.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' })}</span>
 
                 <div className="relative mt-4 mb-2 ml-auto w-[367px] h-[85px]">
                     <Image
@@ -56,7 +56,7 @@ export const Pdf: React.FC<PdfProps> = ({ userData, generatePDF, contentRef }) =
                 </div>
 
 
-                <footer className='flex flex-col text-center text-xs'>
+                <footer className='flex flex-col text-center text-sm'>
                     <p>Vidraçaria Artes Vidros W.R.R - CNPJ: 41.648.910/0001-62</p>
                     <p> Rua Alice dos Santos Peixe, n° 131 - Jardim Selma - São Paulo - São Paulo - CEP: 04431-140</p>
                     <p>www.artesvidroswrr.com.br / artesvidroswrr@gmail.com</p>
